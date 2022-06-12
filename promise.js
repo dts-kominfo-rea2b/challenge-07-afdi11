0b94792ca5cca074c7ee2046a6eff76b0055ae5d
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2022-06-12 22:43:07 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,47 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emotion) => {
+  return new Promise((resolve, reject) => {
+    if (emotion == "") {
+      reject("Invalid Data!");
+    } else if (emotion == "marah") {
+      (async () => {
+        const sumIXX = count(await promiseTheaterIXX(), emotion);
+        const sumVGC = count(await promiseTheaterVGC(), emotion);
+
+        resolve(sumIXX + sumVGC);
+      })();
+    } else if (emotion == "tidak marah") {
+      (async () => {
+        const sumIXX = count(await promiseTheaterIXX(), emotion);
+        const sumVGC = count(await promiseTheaterVGC(), emotion);
+
+        resolve(sumIXX + sumVGC);
+      })();
+    }
+  });
+};
+
+function count(data, emotion) {
+  let counter = 0;
+
+  if (emotion == "marah") {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].hasil == "marah") {
+        counter++;
+      }
+    }
+  } else {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].hasil == "tidak marah") {
+        counter++;
+      }
+    }
+  }
+
+  return counter;
+}
 
 module.exports = {
   promiseOutput,
